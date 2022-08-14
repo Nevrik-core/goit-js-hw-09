15cb266771c6f8c4dcb52f0fbe1ef6e1724bb042
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2022-08-14 16:22:46 +0300

    let timediff just dissapear

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -66,7 +66,7 @@ function convertMs(ms) {
 }
 
 function timerStart() {
-  let timeDiff = null;
+  let timeDiff;
     const intervalTimerId = setInterval(() => {
         timeDiff = flatpick.selectedDates[0].getTime() - new Date().getTime();
         const { days, hours, minutes, seconds } = convertMs(timeDiff);
