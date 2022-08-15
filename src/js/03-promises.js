2a58d6ad9d2be6175170a811869d3ddad2034de4
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2022-08-15 12:31:18 +0300

    Task 3 complete

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,9 +1,59 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import throttle from 'lodash.throttle';
+import Notiflix from 'notiflix';
+
+const submitBnt = document.querySelector('button');
+const form = document.querySelector('form');
+// console.log(form);
+
+const STORAGE_KEY = 'form-data';
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+// console.log(formData);
+
+form.addEventListener('input', throttle(onInputChange, 500));
+
+
+function onInputChange(e) {
+
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    // console.log(formData);
+    
 }
-so
+
+submitBnt.addEventListener('submit', createPromise);
+
+function createPromise(position, delay) {
+
+  localStorage.removeItem(STORAGE_KEY);
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+let delayValue = Number(formData.delay);
+
+
+for (let i = 1; i <= formData.amount; i++) {
+  const promise = createPromise(i, delayValue);
+  console.log(promise);
+  promise
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+    
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+
+  delayValue += Number(formData.step);
+ 
+}
+
